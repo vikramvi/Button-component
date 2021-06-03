606bbb2283a49e2d599219f496677b041b936803
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2021-06-03 21:23:37 +0530

    initial commit with few working buttons created using button component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color:${(props) => theme[props.theme].default};
+  color: white;
+  padding: 5px 15px;
+  border-radius: 5px;
+  outline: 0;
+  text-transform: uppercase;
+  margin: 10px 0px;
+  cursor: pointer;
+  box-shadow: 0px 2px 2px lightgray;
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
+`;
+
+const theme = {
+  blue: {
+    default: "#3f51b5",
+    hover: "#283593"
+  },
+  pink: {
+    default: "#e91e63",
+    hover: "#ad1457"
+  }
+};
+
+Button.defaultProps = {
+  theme: "blue"
+};
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Button>Jai Ho</Button>
+      </div>
+      <div>
+        <Button disabled>Disabled Jai Ho</Button>
+      </div>
+      <div>
+        <Button theme="pink">
+          Pink theme
+        </Button>
+      </div>
+    </>
   );
 }
 
