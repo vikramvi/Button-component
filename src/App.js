368ca98a061bb6d879fe64b4a12ab2796ed72718
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2021-06-07 21:46:57 +0530

    all button types are completed

--- src/App.js
+++ src/App.js
@@ -1,93 +1,139 @@
-import styled from "styled-components";
-
+import styled, { css } from "styled-components";
+import LocalGroceryStoreIcon from '@material-ui/icons/LocalGroceryStore';
 
 const Button = styled.button`
-  width: 88px;
-  height: 36px;
-  background:${(props) => buttonTheme[props.buttonTheme].backgroundValue}; 
-  box-shadow:${(props) => buttonTheme[props.buttonTheme].boxShadowValue};
-  border:${(props) => buttonTheme[props.buttonTheme].borderValue};
-  color:${(props) => buttonTheme[props.buttonTheme].colorValue};
-  border-radius: 6px;
-  margin: 20px 50px;
-`;
-
-const theme = {
-  blue: {
-    default: "#3f51b5",
-    hover: "#283593"
-  },
-  pink: {
-    default: "#e91e63",
-    hover: "#ad1457"
+      
+  ${props => props.shouldHover
+    ? '&:hover { background: yellow }'
+    : ''
   }
-};
+
+  ${props => props.shouldHover
+    ? '&:focus { background: yellow }'
+    : ''
+  }
+
+  ${(props) => {
+    const concatenatedTheme = Object.assign(
+      {},
+      defaultButtonTheme,
+      buttonTheme[props.buttonTheme]
+    );
+
+    return css`
+        width: ${concatenatedTheme.widthValue};
+        height: ${concatenatedTheme.heightValue};
+        background: ${concatenatedTheme.backgroundValue};
+        box-shadow: ${concatenatedTheme.boxShadowValue};
+        border: ${concatenatedTheme.borderValue};
+        color: ${concatenatedTheme.colorValue};
+        border-radius: ${concatenatedTheme.borderRadiusValue};
+        margin: 20px 30px;
+    `;
+  }
+  }
+
+`;
 
 Button.defaultProps = {
   buttonTheme: "defaultButtonTheme"
 };
 
+
+const defaultButtonTheme = {
+  borderValue: "0",
+  boxShadowValue: "0",
+  backgroundValue: "0",
+  widthValue: "88px",
+  heightValue: "36px",
+  borderRadiusValue: "6px",
+  colorValue: "#FFFFFF"
+}
+
 const buttonTheme = {
-  defaultButtonTheme: {
-    borderValue: "0",
-    boxShadowValue: "0",
-    backgroundValue: "0"
-  },
   defaultButton: {
     backgroundValue: "#E0E0E0",
     boxShadowValue: "0px 2px 3px rgba(51, 51, 51, 0.2)",
-    borderValue: "0",
     colorValue: "#3F3F3F"
   },
   hoverFocusType1Button: {
     backgroundValue: "#AEAEAE",
     boxShadowValue: "0px 2px 3px rgba(51, 51, 51, 0.2)",
-    borderValue: "0",
     colorValue: "#3F3F3F"
   },
   OutlineVariantButton: {
     backgroundValue: "0",
     borderValue: "1px solid #3D5AFE",
-    boxShadowValue: "0",
     colorValue: "#3D5AFE"
   },
   hoverFocusType2Button: {
     backgroundValue: "rgba(41, 98, 255, 0.1)",
-    boxShadowValue: "0",
     borderValue: "1px solid #3D5AFE",
     colorValue: "#3D5AFE"
   },
   textVariantButton: {
-    backgroundValue: "0",
-    borderValue: "0",
-    boxShadowValue: "0",
     colorValue: "#3D5AFE"
   },
   hoverFocusType3Button: {
     backgroundValue: "rgba(41, 98, 255, 0.1)",
-    boxShadowValue: "0",
-    borderValue: "0",
     colorValue: "#3D5AFE"
   },
   disableShadowButton: {
     backgroundValue: "#3D5AFE",
-    boxShadowValue: "0",
-    borderValue: "0",
     colorValue: "#FFFFFF"
   },
   disabledButton: {
     backgroundValue: "#E0E0E0",
-    boxShadowValue: "0",
-    borderValue: "0",
     colorValue: "#9E9E9E",
   },
   textVariantDisabledButton: {
-    backgroundValue: "0",
-    boxShadowValue: "0",
-    borderValue: "0",
     colorValue: "#9E9E9E"
+  },
+  startIconButton: {
+    widthValue: "105px",
+    backgroundValue: "#2962FF",
+    boxShadowValue: "0px 2px 3px rgba(0, 49, 202, 0.2)"
+  },
+  endIconButton: {
+    widthValue: "105px",
+    backgroundValue: "#2962FF",
+    boxShadowValue: "0px 2px 3px rgba(0, 49, 202, 0.2)"
+  },
+  smallButton: {
+    widthValue: "73px",
+    heightValue: "32px",
+    backgroundValue: "#2962FF",
+    boxShadowValue: "0px 2px 3px rgba(0, 49, 202, 0.2)"
+  },
+  mediumButton: {
+    widthValue: "81px",
+    heightValue: "36px",
+    backgroundValue: "#2962FF",
+    boxShadowValue: "0px 2px 3px rgba(0, 49, 202, 0.2)"
+  },
+  largeButton: {
+    widthValue: "93px",
+    heightValue: "42px",
+    backgroundValue: "#2962FF",
+    boxShadowValue: "0px 2px 3px rgba(0, 49, 202, 0.2)"
+  },
+  defaultColorButton: {
+    backgroundValue: "#E0E0E0",
+    boxShadowValue: "0px 2px 3px rgba(51, 51, 51, 0.2)",
+    colorValue: "#3F3F3F"
+  },
+  primaryColorButton: {
+    backgroundValue: "#2962FF",
+    boxShadowValue: "0px 2px 3px rgba(41, 98, 255, 0.2)"
+  },
+  secondaryColorButton: {
+    backgroundValue: "#455A64",
+    boxShadowValue: "0px 2px 3px rgba(69, 90, 100, 0.2)"
+  },
+  dangerColorButton: {
+    backgroundValue: "#D32F2F",
+    boxShadowValue: "0px 2px 3px rgba(211, 47, 47, 0.2)"
   }
-
 }
 
 
@@ -122,6 +168,33 @@ function App() {
         </div>
         <div>
           <Button buttonTheme="textVariantDisabledButton">Default</Button>
+        </div>
+        <div>
+          <Button buttonTheme="startIconButton"><LocalGroceryStoreIcon></LocalGroceryStoreIcon>Default</Button>
+        </div>
+        <div>
+          <Button buttonTheme="endIconButton">Default<LocalGroceryStoreIcon></LocalGroceryStoreIcon></Button>
+        </div>
+        <div>
+          <Button buttonTheme="smallButton">Default</Button>
+        </div>
+        <div>
+          <Button buttonTheme="mediumButton">Default</Button>
+        </div>
+        <div>
+          <Button buttonTheme="largeButton">Default</Button>
+        </div>
+        <div>
+          <Button buttonTheme="defaultColorButton">Default</Button>
+        </div>
+        <div>
+          <Button buttonTheme="primaryColorButton" shouldHover>Default</Button>
+        </div>
+        <div>
+          <Button buttonTheme="secondaryColorButton" shouldHover>Default</Button>
+        </div>
+        <div>
+          <Button buttonTheme="dangerColorButton" shouldHover>Default</Button>
         </div>
       </div>
     </>
